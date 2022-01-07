99f1ec3b6a81e7c85448c34c81751e8e98cd5bb4
Rahoom-Ajeena <96892118+Rahoom-Ajeena@users.noreply.github.com>
2022-01-07 13:25:59 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@
 
 location = 'Newmarket' # PUT YOU'RE CITY
 countryName = 'Canada' # PUT YOU'RE COUNTRY
+hour12 = 'false' # false = 24 hour clock, true = 12 hour clock [must be lower case] *SYNCS WITH PC SO PICK THE PC CLOCK TYPE*
